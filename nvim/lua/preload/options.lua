7d49ee62a59ef5b8ba513a3f049275cd8343c57f
Vasileios Bardakos <vasilisbardakos@gmail.com>
2024-03-05 12:49:19 +0200

    autoindent and shiftswift plugin

--- nvim/lua/preload/options.lua
+++ nvim/lua/preload/options.lua
@@ -6,10 +6,10 @@ vim.g.maplocalleader = " "
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
--- indenting
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+-- indenting // added plugin
+-- vim.opt.tabstop = 4
+-- vim.opt.softtabstop = 4
+-- vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
