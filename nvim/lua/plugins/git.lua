2de677f2b00c7414c960af3f3c34985b0dd46bbb
Vasileios Bardakos <vasilisbardakos@gmail.com>
2024-03-05 13:38:53 +0200

    add lazy git

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -2,4 +2,18 @@ return {
     -- Git related plugins
     'tpope/vim-fugitive',
     'tpope/vim-rhubarb',
+    {
+	"kdheepak/lazygit.nvim",
+	cmd = {
+	    "LazyGit",
+	    "LazyGitConfig",
+	    "LazyGitCurrentFile",
+	    "LazyGitFilter",
+	    "LazyGitFilterCurrentFile",
+	},
+	-- optional for floating window border decoration
+	dependencies = {
+	    "nvim-lua/plenary.nvim",
+	},
+    },
 }
