0db7bdc568d91b8c26269baeacac56f4ff1591c3
Vasileios Bardakos <vasilisbardakos@gmail.com>
2024-03-10 18:17:19 +0200

    add devicons in colors

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,6 +1,7 @@
 return {
-    "bluz71/vim-nightfly-colors",
-    "folke/tokyonight.nvim",
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    "Mofiqul/dracula.nvim"
+	"bluz71/vim-nightfly-colors",
+	"folke/tokyonight.nvim",
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	"Mofiqul/dracula.nvim",
+	"nvim-tree/nvim-web-devicons",
 }
