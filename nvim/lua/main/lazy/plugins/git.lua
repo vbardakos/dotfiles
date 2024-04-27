84f8519dc741a0593206d59f0603ba0ef37e98b2
Vasileios Bardakos <vasilisbardakos@gmail.com>
2024-04-27 19:14:29 +0300

    feat: fix gitsigns

--- nvim/lua/main/lazy/plugins/git.lua
+++ nvim/lua/main/lazy/plugins/git.lua
@@ -1,14 +1,16 @@
 return {
   "tpope/vim-fugitive",
   "tpope/vim-rhubarb",
-  "lewis6991/gitsigns.nvim",
-  opts = {
-    signs = {
-      add = { text = "" },
-      change = { text = "󰜥" },
-      delete = { text = "" },
-      topdelete = { text = "" },
-      changedelete = { text = "󰜥" },
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = {
+      signs = {
+        add = { text = "" },
+        change = { text = "󰜥" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "󰜥" },
+      },
     },
   },
 }
