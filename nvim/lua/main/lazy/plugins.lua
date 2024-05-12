3d5c74e16d84728b0517f79972c3975a6353120f
Vasileios Bardakos <vasilisbardakos@gmail.com>
2024-05-12 22:22:17 +0300

    feat: add yaml support

--- nvim/lua/main/lazy/plugins.lua
+++ nvim/lua/main/lazy/plugins.lua
@@ -1,4 +1,8 @@
 return {
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
   "fladson/vim-kitty",
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
 }
--- nvim/lua/main/lazy/plugins/linting.lua
+++ nvim/lua/main/lazy/plugins/linting.lua
@@ -1,16 +1,17 @@
 return {
 
   { -- Linting
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
-      local lint = require 'lint'
+      local lint = require "lint"
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
-        python = { 'ruff' },
-        json = { 'jsonlint' },
-        text = { 'vale' },
-        rst = { 'vale' },
+        markdown = { "markdownlint" },
+        python = { "ruff" },
+        json = { "jsonlint" },
+        text = { "vale" },
+        rst = { "vale" },
+        yaml = { "yamllint" },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -47,11 +48,11 @@ return {
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
         group = lint_augroup,
         callback = function()
-          require('lint').try_lint()
+          require("lint").try_lint()
         end,
       })
     end,
