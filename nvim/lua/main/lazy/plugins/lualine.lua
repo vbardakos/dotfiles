458d72de0da630a7a8a2ac21d903a35a060dc3a4
Vasileios Bardakos <vasilisbardakos@gmail.com>
2024-05-21 16:52:30 +0300

    feat(lualine): customize lualine

--- nvim/lua/main/lazy/plugins/lualine.lua
+++ nvim/lua/main/lazy/plugins/lualine.lua
@@ -1,13 +1,49 @@
+local lsp_name = function()
+  -- local msg = "No Active Lsp"
+  local msg = ""
+  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+  local clients = vim.lsp.get_active_clients()
+  if next(clients) == nil then
+    return msg
+  end
+  for _, client in ipairs(clients) do
+    local filetypes = client.config.filetypes
+    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+      return client.name
+    end
+  end
+  return msg
+end
+
 return {
-  -- Set lualine as statusline
   "nvim-lualine/lualine.nvim",
-  -- See `:help lualine.txt`
   opts = {
     options = {
       icons_enabled = true,
-      theme = "dracula",
+      theme = "rose-pine",
       component_separators = "|",
       section_separators = "",
+    },
+    sections = {
+      -- stylua: ignore
+      lualine_a = { { "mode", fmt = function(str) return str:sub(1, 1) end, } },
+      lualine_b = {
+        {
+          "diagnostics",
+          sources = { "nvim_diagnostic" },
+          symbols = { error = " ", warn = " ", info = " " },
+        },
+        { lsp_name, icon = " " },
+      },
+      lualine_c = {
+        -- stylua: ignore
+        { "filetype", fmt = function(_) return vim.fn.fnamemodify(vim.fn.expand("%:p"), ":~:.") end, },
+      },
+      lualine_x = {
+        { "branch", icon = "" },
+        { "diff", symbols = { added = " ", modified = "󰝤 ", removed = " " } },
+        -- { "o:encoding", fmt = string.upper },
+      },
     },
   },
 }
