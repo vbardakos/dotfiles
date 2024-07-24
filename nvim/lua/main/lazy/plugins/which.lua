238c6813961ee0a7e915482bf6deafb254ea5976
Vasileios Bardakos <vasilisbardakos@gmail.com>
2024-07-24 17:38:42 +0300

    feat(update): which update depricated conf

--- nvim/lua/main/lazy/plugins/which.lua
+++ nvim/lua/main/lazy/plugins/which.lua
@@ -1,17 +1,16 @@
-return { -- Useful plugin to show you pending keybinds.
+return {
   "folke/which-key.nvim",
-  event = "VimEnter", -- Sets the loading event to 'VimEnter'
-  config = function() -- This is the function that runs, AFTER loading
-    require("which-key").setup()
-
-    -- Document existing key chains
-    require("which-key").register {
-      ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-      ["<leader>h"] = { name = "[H]arpoon", _ = "which_key_ignore" },
-      ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-      ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-      ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-      ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+  event = "VimEnter",
+  config = function()
+    which = require "which-key"
+    which.setup()
+    which.add {
+      { "<leader>c", group = "[C]ode" },
+      { "<leader>h", group = "[H]arpoon" },
+      { "<leader>d", group = "[D]ocument" },
+      { "<leader>r", group = "[R]ename" },
+      { "<leader>s", group = "[S]earch" },
+      { "<leader>w", group = "[W]orkspace" },
     }
   end,
 }
