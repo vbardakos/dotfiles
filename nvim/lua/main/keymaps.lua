5200249de468928eb63b2cafe9c8d1ed548dfe3f
Vasileios Bardakos <vasilisbardakos@gmail.com>
2024-08-14 09:15:08 +0300

    remove autocmds from keymaps

--- nvim/lua/main/keymaps.lua
+++ nvim/lua/main/keymaps.lua
@@ -37,47 +37,16 @@ vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper win
 
 vim.keymap.set("n", "<leader>m", "<CMD>w | make %<CR>", { desc = "[M]ake current file" })
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
+vim.keymap.set(
+  "n",
+  "<leader>td",
+  "<CMD>DiagnosticBufferToggle<CR>",
+  { desc = "Toggle Buffer [D]iagnostics" }
+)
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
-  desc = "Highlight when yanking (copying) text",
-  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  desc = "nvim make automation",
-  group = vim.api.nvim_create_augroup("automake", { clear = true }),
-  pattern = { "python" },
-  callback = function(opts)
-    if opts.match == "python" then
-      vim.bo.makeprg = "python"
-    elseif opts.match == "rust" then
-      local fname = vim.fn.fnamemodify(vim.fn.expand "%:t", ":r")
-      -- local dest = vim.fn.expand("%:r").
-      -- --manifest-path
-      if fname == "main" then
-        vim.bo.makerpg = "cargo run"
-      else
-        vim.bo.makerpg = "cargo test " .. fname
-      end
-    end
-  end,
-})
-
--- TODO: Create current buffer DiagnosticToggle
-vim.api.nvim_create_user_command("DiagnosticToggle", function()
-  local config = vim.diagnostic.config
-  local vt = config().virtual_text
-  config {
-    virtual_text = not vt,
-    underline = not vt,
-    signs = not vt,
-  }
-end, { desc = "toggle diagnostic" })
+vim.keymap.set(
+  "n",
+  "<leader>tm",
+  "<CMD>MarkdownPreviewToggle<CR>",
+  { desc = "Toggle [M]arkdown Preview" }
+)
