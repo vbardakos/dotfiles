9ba32327c746ad5efe00005ee91b77a18c8431a2
Vasileios Bardakos <vasilisbardakos@gmail.com>
2024-12-29 15:42:21 +0200

    feat(nvim): fix harpoon keymaps

--- nvim/lua/main/lazy/plugins/harpoon.lua
+++ nvim/lua/main/lazy/plugins/harpoon.lua
@@ -7,17 +7,18 @@ return {
     harpoon:setup()
 
     -- stylua: ignore start
-    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "[A]dd Harpoon"})
-    vim.keymap.set("n", "<leader>h<leader>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggle Harpoon" })
+    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "[A]dd Harpoon"})
+    vim.keymap.set("n", "<Tab><leader>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Toggle Harpoon" })
 
-    vim.keymap.set({ "n", "t" }, "<leader>hh", function() harpoon:list():select(1) end, { desc = "Harpoon [1]"})
-    vim.keymap.set({ "n", "t" }, "<leader>hj", function() harpoon:list():select(2) end, { desc = "Harpoon [2]"})
-    vim.keymap.set({ "n", "t" }, "<leader>hk", function() harpoon:list():select(3) end, { desc = "Harpoon [3]"})
-    vim.keymap.set({ "n", "t" }, "<leader>hl", function() harpoon:list():select(4) end, { desc = "Harpoon [4]"})
+    vim.keymap.set({ "n", "t" }, "<Tab>a", function() harpoon:list():select(1) end, { desc = "Harpoon [1]"})
+    vim.keymap.set({ "n", "t" }, "<Tab>s", function() harpoon:list():select(2) end, { desc = "Harpoon [2]"})
+    vim.keymap.set({ "n", "t" }, "<Tab>d", function() harpoon:list():select(3) end, { desc = "Harpoon [3]"})
+    vim.keymap.set({ "n", "t" }, "<Tab>f", function() harpoon:list():select(4) end, { desc = "Harpoon [4]"})
+    vim.keymap.set({ "n", "t" }, "<Tab>g", function() harpoon:list():select(4) end, { desc = "Harpoon [5]"})
 
     -- Toggle previous & next buffers stored within Harpoon list
-    -- vim.keymap.set("n", "<leader>h<>", function() harpoon:list():prev() end, { desc = "Harpoon Prev" })
-    vim.keymap.set("n", "<leader>h<Tab>", function() harpoon:list():next() end, { desc = "Harpoon Next" })
+    vim.keymap.set("n", "<Tab>p", function() harpoon:list():prev() end, { desc = "Harpoon Prev" })
+    vim.keymap.set("n", "<Tab>n", function() harpoon:list():next() end, { desc = "Harpoon Next" })
     -- stylua: ignore end
   end,
 }
