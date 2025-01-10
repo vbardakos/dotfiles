c247447b7482ca1ab2371e25a2abff8e51a9e567
Vasileios Bardakos <vasilisbardakos@gmail.com>
2025-01-10 17:57:43 +0200

    feat(virtual_text): improve blending and coloring

--- nvim/lua/main/lazy/plugins/colors.lua
+++ nvim/lua/main/lazy/plugins/colors.lua
@@ -6,12 +6,13 @@ return {
     styles = {
       bold = true,
       italic = true,
+      transparency = true,
     },
   },
   init = function()
     local palette = require "rose-pine.palette"
 
-    vim.cmd.colorscheme "rose-pine"
+    vim.cmd.colorscheme "rose-pine-moon"
     vim.cmd.hi "Comment gui=none"
     local highlight = "#26233a"
 
@@ -21,5 +22,17 @@ return {
     -- deep orange #ff966c
     -- stylua: ignore
     vim.api.nvim_set_hl( 0, "CursorLineNR", { fg = palette.gold, bg = highlight, bold = true })
+
+    local set_virtual_text_hl = function(level, fg)
+      local capitalised = level:sub(1, 1):upper() .. level:sub(2):lower()
+      local name = "DiagnosticVirtualText" .. capitalised
+      vim.api.nvim_set_hl(0, name, { fg = fg, italic = true, blend = 100 })
+    end
+
+    -- lsp virtual text override
+    set_virtual_text_hl("hint", palette.highlight_low)
+    set_virtual_text_hl("info", palette.highlight_low)
+    set_virtual_text_hl("warn", palette.highlight_med)
+    set_virtual_text_hl("error", palette.highlight_high)
   end,
 }
--- nvim/lua/main/lazy/plugins/lsp.lua
+++ nvim/lua/main/lazy/plugins/lsp.lua
@@ -87,6 +87,19 @@ return { -- LSP Configuration & Plugins
       end,
     })
 
+    vim.diagnostic.config {
+      virtual_text = { prefix = "●", spacing = 4, source = "if_many", hl_mode = "blend" },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.HINT] = " ",
+          [vim.diagnostic.severity.INFO] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.ERROR] = " ",
+        },
+      },
+      severity_sort = true,
+    }
+
     -- LSP servers and clients are able to communicate to each other what features they support.
     --  By default, Neovim doesn't support everything that is in the LSP specification.
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
@@ -95,16 +108,21 @@ return { -- LSP Configuration & Plugins
     capabilities =
       vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-    -- Enable the following language servers
-    --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-    --
-    --  Add any additional override configuration in the following tables. Available keys are:
-    --  - cmd (table): Override the default command used to start the server
-    --  - filetypes (table): Override the default list of associated filetypes for the server
-    --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    --  - settings (table): Override the default settings passed when initializing the server.
-    --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
+      pyright = {
+        settings = {
+          pyright = {
+            -- Using Ruff's import organizer
+            disableOrganizeImports = true,
+          },
+          python = {
+            analysis = {
+              -- Ignore all files for analysis to exclusively use Ruff for linting
+              ignore = { "*" },
+            },
+          },
+        },
+      },
       ruff = {
         -- https://docs.astral.sh/ruff/editors/settings
         init_options = {
