175de8931f3cdecbd2b141bd8ea314aad874caf0
Vasileios Bardakos <vasilisbardakos@gmail.com>
2025-03-11 16:01:03 +0200

    feat(oil): change into float

--- nvim/lua/main/lazy/plugins/oil.lua
+++ nvim/lua/main/lazy/plugins/oil.lua
@@ -2,8 +2,23 @@ return {
   "stevearc/oil.nvim",
   config = function()
     require("oil").setup {
+      default_file_explorer = true,
       columns = { "icon" }, -- "size" => filesize
       win_options = { signcolumn = "yes" },
+      -- Configuration for the floating window in oil.open_float
+      float = {
+        open_by_default = true,
+        -- Padding around the floating window
+        padding = 2,
+        -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        max_width = 0.7,
+        max_height = 0.8,
+        border = "rounded",
+        win_options = {
+          signcolumn = "yes",
+        },
+        preview_split = "auto",
+      },
       view_options = {
         is_hidden_file = function(name, _)
           -- Table-based lookup for exact matches
@@ -50,21 +65,20 @@ return {
         end,
       },
     }
-  vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "[O]il File Explorer" })
+    vim.keymap.set("n", "<leader>o", "<CMD>Oil --float<CR>", { desc = "[O]il File Explorer" })
 
-  vim.api.nvim_create_autocmd("FileType", {
-    desc = "enable tmux navigation in oil",
-    group = vim.api.nvim_create_augroup("OilTmuxNav", { clear = true }),
-    pattern = { "oil" },
-    callback = function()
-      vim.schedule(function ()
+    vim.api.nvim_create_autocmd("FileType", {
+      desc = "enable tmux navigation in oil",
+      group = vim.api.nvim_create_augroup("OilTmuxNavigation", { clear = true }),
+      pattern = { "oil" },
+      callback = function()
+        vim.schedule(function()
           vim.keymap.set("n", "<C-h>", "<CMD>TmuxNavigateLeft<cr>", { buffer = true })
           vim.keymap.set("n", "<C-j>", "<CMD>TmuxNavigateDown<cr>", { buffer = true })
           vim.keymap.set("n", "<C-k>", "<CMD>TmuxNavigateUp<cr>", { buffer = true })
           vim.keymap.set("n", "<C-l>", "<CMD>TmuxNavigateRight<cr>", { buffer = true })
-      end)
-    end,
-  })
-
-  end
+        end)
+      end,
+    })
+  end,
 }
