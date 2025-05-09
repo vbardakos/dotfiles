e6469b7d9f182d4ef21c367534ca5706d7389680
Vasileios Bardakos <vasilisbardakos@gmail.com>
2025-05-09 21:28:50 +0300

    fix(oil): revert float changes

--- nvim/lua/main/lazy/plugins/oil.lua
+++ nvim/lua/main/lazy/plugins/oil.lua
@@ -5,20 +5,6 @@ return {
       default_file_explorer = true,
       columns = { "icon" }, -- "size" => filesize
       win_options = { signcolumn = "yes" },
-      -- Configuration for the floating window in oil.open_float
-      float = {
-        open_by_default = true,
-        -- Padding around the floating window
-        padding = 2,
-        -- max_width and max_height can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
-        max_width = 0.7,
-        max_height = 0.8,
-        border = "rounded",
-        win_options = {
-          signcolumn = "yes",
-        },
-        preview_split = "auto",
-      },
       view_options = {
         is_hidden_file = function(name, _)
           -- Table-based lookup for exact matches
@@ -65,7 +51,7 @@ return {
         end,
       },
     }
-    vim.keymap.set("n", "<leader>o", "<CMD>Oil --float<CR>", { desc = "[O]il File Explorer" })
+    vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>", { desc = "[O]il File Explorer" })
 
     vim.api.nvim_create_autocmd("FileType", {
       desc = "enable tmux navigation in oil",
